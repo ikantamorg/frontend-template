d3d493e7e15138c2bef8016cdb47f4c9884f9077
Mahmoud Elmahdi <elmahdi.mahmoud@gmail.com>
2013-11-07 17:02:19 +0300

    Frontend template

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+/*! 
+ *  Written for: ?
+ *  Author: @IkantamCorp - @ElmahdiMahmoud
+ *  Further changes: @ElmahdiMahmoud
+ *  Updated: 07.11.13
+ */
+
+ (function($) { 
+ $(document).ready(function() { 
+	
+ }); //end doc ready
+})(jQuery); 
