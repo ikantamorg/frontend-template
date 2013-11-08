34c94c321ed964121fa567d913c0a78a6a428153
ElmahdiMahmoud <elmahdi.mahmoud@gmail.com>
2013-11-08 08:39:30 +0300

    Updated custom.js
    - File information
    - Basic plugin pattern snippet

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,30 @@
 /*! 
  *  Written for: ?
  *  Author: @IkantamCorp - @ElmahdiMahmoud
- *  Further changes: @ElmahdiMahmoud
- *  Updated: 07.11.13
+ *  Further changes: @author
+ *  Last modified date: 09.11.13
  */
 
  (function($) { 
  $(document).ready(function() { 
+	
+	/*!
+	 * Basic plugin pattern
+	 */
+	/*
+	// self-invoking
+	;(function ($, window, document, undefined) {
+	    $.fn.plugiName = function () {
+	        return this.each(function () {
+	            var $this = $(this);
+	            //plugin logic
+	        });
+	    }
+	})(jQuery, window, document);
+	//fire the plugin
+	$('selector').plugiName();
+	*/
+
 	
  }); //end doc ready
 })(jQuery); 
