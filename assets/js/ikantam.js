55baee48b2e7f1a0cedb553f5d4d1071e27639be
ElmahdiMahmoud <elmahdi.mahmoud@gmail.com>
2013-12-02 10:32:49 +0300

    Update ikantam.js
    add stepTo plugin

--- assets/js/ikantam.js
+++ assets/js/ikantam.js
@@ -2,11 +2,24 @@
  *  Written for: ?
  *  Author: @IkantamCorp - @ElmahdiMahmoud
  *  Further changes: @author
- *  Last modified date: 09.11.13
+ *  Last modified date: 02.12.13
  */
 
  (function($) { 
  $(document).ready(function() { 
+	
+	$.fn.stepTo = function () {
+		return this.each(function () {
+		var $this = $(this);
+		$this.on('click', function(){
+		$('html,body').animate({
+			scrollTop: $(this.hash).offset().top - 85
+		}, 1000);
+		return false;
+		});
+		});
+	}
+	$('.step-to').stepTo();
 	
 	/*!
 	 * Basic plugin pattern
